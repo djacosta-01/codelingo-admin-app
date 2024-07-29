2eb720dc92fe4a9b845b4c74434d0885f3a18690
djacosta-01 <djacosta.07@gmail.com>
2024-07-29 14:44:44 -0700

    stashing changes before I move knowledge graph data to supabase

--- client/src/Components/KnowledgeGraph/MockKnowledgeGraph.jsx
+++ client/src/Components/KnowledgeGraph/MockKnowledgeGraph.jsx
@@ -2,35 +2,13 @@ import { Box, Button, Dialog, DialogContent, DialogTitle, TextField } from '@mui
 import { useState, useEffect, useCallback } from 'react'
 import { ReactFlow, addEdge, applyEdgeChanges, applyNodeChanges } from 'reactflow'
 import NavbarWithSideMenu from '../NavbarAndSideMenu/NavbarWithSideMenu'
+import { supabase } from '../../supabaseClient/supabaseClient'
 import {
-  initialEdges,
-  initialNodes,
+  // initialEdges,
+  // initialNodes,
   formatNodeData,
   formatEdgeData,
 } from './scripts/initialMockValues'
-
-// FOR TESTING
-const initializeGraph = async () => {
-  // console.log('initializing graph')
-  try {
-    const response = await fetch('http://localhost:5000/store-kg', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        nodes: initialNodes,
-        edges: initialEdges,
-        class_name: 'CS-101',
-        graph_name: 'class_avg_kg',
-      }),
-    })
-    const data = await response.json()
-  } catch (error) {
-    console.error(error)
-  }
-}
-initializeGraph()
 
 const MockKnowledgeGraph = () => {
   const [nodes, setNodes] = useState([])
@@ -53,20 +31,37 @@ const MockKnowledgeGraph = () => {
 
   /* source: https://reactflow.dev/learn/concepts/core-concepts */
   const onNodesChange = useCallback(
-    changes => setNodesForReactFlow(nds => applyNodeChanges(changes, nds)),
+    changes => {
+      // console.log('onNodesChange')
+      // console.log(changes)
+      setNodesForReactFlow(nds => applyNodeChanges(changes, nds))
+    },
     [setNodesForReactFlow]
   )
 
   const onEdgesChange = useCallback(
-    changes => setEdgesForReactFlow(eds => applyEdgeChanges(changes, eds)),
+    changes => {
+      // console.log('onEdgesChange')
+      // console.log(changes)
+      setEdgesForReactFlow(eds => applyEdgeChanges(changes, eds))
+    },
     [setEdgesForReactFlow]
   )
   // handles edge connection
   const onConnect = useCallback(
-    connection => setEdgesForReactFlow(eds => addEdge({ ...connection, animated: true }, eds)),
+    connection => {
+      console.log('onConnect')
+      console.log(connection)
+      const { source, target } = connection
+      const valid = checkIfValid([[source, target]])
+      console.log('valid check')
+      console.log(valid)
+      if (valid) setEdgesForReactFlow(eds => addEdge({ ...connection, animated: true }, eds))
+    },
     [setEdgesForReactFlow]
   )
 
+  // checking if edge connections are valid
   // move this to scripts folder later??
   const checkIfValid = useCallback(async edgesToAdd => {
     const response = await fetch('http://localhost:5000/classes/CS-101/update-class-graph', {
@@ -85,9 +80,11 @@ const MockKnowledgeGraph = () => {
       alert(message)
     }
     setFetchTrigger(prev => prev + 1)
+    return cycleFormed
     // console.log(data)
   }, [])
 
+  // fetching graph data
   useEffect(() => {
     const fetchGraphData = async () => {
       // console.log('fetching graph data')
@@ -100,12 +97,14 @@ const MockKnowledgeGraph = () => {
     fetchGraphData()
   }, [fetchTrigger])
 
+  // setting nodes and edges for react flow
   useEffect(() => {
     // console.log('setting nodes and edges for react flow')
     setNodesForReactFlow(formatNodeData(nodes))
     setEdgesForReactFlow(formatEdgeData(edges))
   }, [nodes, edges])
 
+  // form validation
   const addDataToGraph = _event => {
     _event.preventDefault()
     const regex = /\s+/g
