4b521ae080719e5c9e41e27b53ba79717cdd61da
djacosta-01 <djacosta.07@gmail.com>
2024-08-21 18:22:29 -0700

    have logic for resetting password but need a server

--- client/src/Components/Authentication/Login/Login.jsx
+++ client/src/Components/Authentication/Login/Login.jsx
@@ -3,19 +3,41 @@ import { useState } from 'react'
 // import axios from 'axios'
 import './Login.css'
 import { useNavigate, Link } from 'react-router-dom'
-import { Box, TextField, Typography, InputAdornment } from '@mui/material'
+import {
+  Box,
+  TextField,
+  Typography,
+  InputAdornment,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  Button,
+} from '@mui/material'
 import AccountCircle from '@mui/icons-material/AccountCircle'
 import LockIcon from '@mui/icons-material/Lock'
 import { supabase } from '../../../supabaseClient/supabaseClient.js'
 
 const Login = () => {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [user, setUser] = useState(null)
+  const [open, setOpen] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   const navigate = useNavigate()
 
+  const handleDialogOpen = () => {
+    setOpen(true)
+  }
+
+  const handleDialogClose = () => {
+    setEmail('')
+    setOpen(false)
+  }
+  const handlePasswordReset = async () => {
+    await supabase.auth.resetPasswordForEmail(email, {
+      redirectTo: 'http://localhost:3000/reset-password',
+    })
+  }
   const handleSubmit = async event => {
     event.preventDefault()
     const { data, error } = await supabase.auth.signInWithPassword({ email, password })
@@ -30,7 +52,7 @@ const Login = () => {
   }
 
   return (
-    <div className="login-container">
+    <Box className="login-container" sx={{ display: 'flex', gap: 2 }}>
       <img src={logoImage} alt="CodeLingo" width="200" height="200" />
       <h2>Log in to your Educator Account</h2>
       <form onSubmit={handleSubmit}>
@@ -66,9 +88,10 @@ const Login = () => {
         />
         <button type="submit">Log In</button>
       </form>
+      {/* Forgot Password Logic */}
       {/* <Typography
         variant="caption"
-        onClick={() => alert('forgot password')}
+        onClick={() => handleDialogOpen()}
         sx={{
           color: 'grey',
           fontWeight: 'bold',
@@ -79,11 +102,37 @@ const Login = () => {
         }}
       >
         Forgot Password?
-      </Typography> */}
-      <p>
+      </Typography>
+      <Dialog open={open} onClose={handleDialogClose} maxWidth="xl">
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <DialogTitle>Reset Password</DialogTitle>
+          <DialogContent sx={{ display: 'flex', flexDirection: 'column' }}>
+            <DialogContentText>
+              Enter your email address below and we will send you a link to reset your password.
+            </DialogContentText>
+            <TextField
+              variant="standard"
+              id="email"
+              type="email"
+              label="Email"
+              value={email}
+              onChange={_event => setEmail(_event.target.value)}
+            />
+          </DialogContent>
+          <Button onClick={() => alert('button clicked')}>Reset Password</Button>
+        </Box>
+      </Dialog> */}
+      {/* Registration */}
+      <Typography>
         Don't have an account? <Link to="/register">Register Here</Link>
-      </p>
-    </div>
+      </Typography>
+    </Box>
   )
 }
 
