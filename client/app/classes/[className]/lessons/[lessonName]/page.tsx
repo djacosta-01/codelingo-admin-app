8c5239e3ae9905dd46ea16b78fd03b3313380f26
djacosta-01 <djacosta.07@gmail.com>
2024-10-05 19:57:52 -0700

    finished getting input from dialog form. working on inserting data to db

--- client/app/classes/[className]/lessons/[lessonName]/page.tsx
+++ client/app/classes/[className]/lessons/[lessonName]/page.tsx
@@ -31,7 +31,7 @@ const Lesson = ({
   }
 }) => {
   const [open, setOpen] = useState<boolean>(false)
-  const [questionType, setQuestionType] = useState<string>('multiple-choice')
+  const [questionType, setQuestionType] = useState<string>('')
   const [questionPrompt, setQuestionPrompt] = useState<string>('')
   const [options, setOptions] = useState({ option1: '', option2: '', option3: '', option4: '' })
   const [correctAnswer, setCorrectAnswer] = useState<string>('')
@@ -50,8 +50,22 @@ const Lesson = ({
 
   const handleDialogClose = () => {
     setOpen(false)
+    // reset form values
+    setQuestionType('')
+    setQuestionPrompt('')
+    setOptions({ option1: '', option2: '', option3: '', option4: '' })
+    setCorrectAnswer('')
   }
 
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    handleDialogClose()
+    console.log('submitting form')
+    console.log('questionType', questionType)
+    console.log('questionPrompt', questionPrompt)
+    console.log('options', options)
+    console.log('correctAnswer', correctAnswer)
+  }
   return (
     <>
       <NavbarWithSideMenu className={params.className} displaySideMenu currentPage="Lessons" />
@@ -87,22 +101,25 @@ const Lesson = ({
           </Tooltip>
         </Box>
         <QuestionDataGrid params={{ className: params.className, lessonName: params.lessonName }} />
-        <Dialog open={open} fullScreen>
+        <Dialog open={open} fullScreen PaperProps={{ component: 'form', onSubmit: submitForm }}>
           <DialogTitle>Add Question</DialogTitle>
           <DialogContent>
-            <form>
-              <Box
-                id="add-question-form"
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  gap: 1,
-                  justifyContent: 'space-evenly',
-                  height: '80vh',
-                  alignItems: 'center',
-                }}
-              >
+            {/* <form onSubmit={submitForm}> */}
+            <Box
+              id="add-question-form"
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 1,
+                justifyContent: 'space-evenly',
+                height: '80vh',
+                alignItems: 'center',
+              }}
+            >
+              <FormControl id-="question-type">
+                <InputLabel>Question Type</InputLabel>
                 <Select
+                  required
                   label="Question Type"
                   variant="standard"
                   sx={{ width: '15em' }}
@@ -111,79 +128,80 @@ const Lesson = ({
                 >
                   <MenuItem value="multiple-choice">Multiple Choice</MenuItem>
                 </Select>
+              </FormControl>
+
+              <TextField
+                required
+                label="Question Prompt"
+                variant="standard"
+                value={questionPrompt}
+                onChange={e => setQuestionPrompt(e.target.value)}
+              />
+              <Box
+                id="options"
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  gap: 3,
+                }}
+              >
                 <TextField
-                  label="Question Prompt"
+                  required
+                  label="Option 1"
+                  name="option1"
                   variant="standard"
-                  value={questionPrompt}
-                  onChange={e => setQuestionPrompt(e.target.value)}
+                  value={options.option1}
+                  onChange={handleOptionInput}
                 />
-                <Box
-                  id="options"
-                  sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    gap: 3,
-                  }}
-                >
-                  <TextField
-                    required
-                    label="Option 1"
-                    name="option1"
-                    variant="standard"
-                    value={options.option1}
-                    onChange={handleOptionInput}
-                  />
-                  <TextField
-                    required
-                    label="Option 2"
-                    name="option2"
-                    variant="standard"
-                    value={options.option2}
-                    onChange={handleOptionInput}
-                  />
-                  <TextField
-                    required
-                    label="Option 3"
-                    name="option3"
-                    variant="standard"
-                    value={options.option3}
-                    onChange={handleOptionInput}
-                  />
-                  <TextField
-                    required
-                    label="Option 4"
-                    name="option4"
-                    variant="standard"
-                    value={options.option4}
-                    onChange={handleOptionInput}
-                  />
-                </Box>
-                <FormControl>
-                  <InputLabel id="correct-answer-label">Correct Answer</InputLabel>
-                  <Select
-                    required
-                    labelId="correct-answer-label"
-                    label="Correct Answer"
-                    variant="standard"
-                    sx={{ width: '15em' }}
-                    value={Object.keys(options).length !== 4 ? 'Correct Answer' : correctAnswer}
-                    onChange={handleCorrectAnswerSelect}
-                  >
-                    <MenuItem>
-                      <em>-- Correct Answer --</em>
-                    </MenuItem>
-                    {Array.from(Object.values(options)).map((option, index) => (
-                      <MenuItem key={index} value={option}>
-                        {option}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
+                <TextField
+                  required
+                  label="Option 2"
+                  name="option2"
+                  variant="standard"
+                  value={options.option2}
+                  onChange={handleOptionInput}
+                />
+                <TextField
+                  required
+                  label="Option 3"
+                  name="option3"
+                  variant="standard"
+                  value={options.option3}
+                  onChange={handleOptionInput}
+                />
+                <TextField
+                  required
+                  label="Option 4"
+                  name="option4"
+                  variant="standard"
+                  value={options.option4}
+                  onChange={handleOptionInput}
+                />
               </Box>
-            </form>
+              <FormControl>
+                <InputLabel id="correct-answer">Correct Answer</InputLabel>
+                <Select
+                  required
+                  labelId="correct-answer"
+                  label="Correct Answer"
+                  variant="standard"
+                  sx={{ width: '15em' }}
+                  value={correctAnswer}
+                  onChange={handleCorrectAnswerSelect}
+                >
+                  {Array.from(Object.values(options)).map((option, index) => (
+                    <MenuItem key={index} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+            {/* </form> */}
           </DialogContent>
           <DialogActions>
             <Button onClick={handleDialogClose}>Cancel</Button>
+            <Button type="submit">Add Question</Button>
           </DialogActions>
         </Dialog>
       </Box>
