0f44b66dc73ff9ec051363d95397e093ced814a1
djacosta-01 <djacosta.07@gmail.com>
2024-11-08 00:55:59 -0800

    added some starter stuff to class settings page

--- client/app/classes/[className]/class-settings/page.tsx
+++ client/app/classes/[className]/class-settings/page.tsx
@@ -1,5 +1,15 @@
+'use client'
+
+import { Box } from '@mui/material'
+import { QrCode2 as QRCode } from '@mui/icons-material'
 const ClassSettings = () => {
-  return <div>Class Settings Page</div>
+  return (
+    <>
+      <Box>QRCode functionality not implemented yet</Box>
+      <QRCode fontSize="large" />
+      <Box></Box>
+    </>
+  )
 }
 
 export default ClassSettings
