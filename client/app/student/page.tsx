cb815cee40cbb55adb2cf7774f03efc569f16a47
djacosta-01 <djacosta.07@gmail.com>
2024-11-14 15:20:36 -0800

    made mock student page for now until mobile app pushes their changes

--- client/app/student/page.tsx
+++ client/app/student/page.tsx
@@ -1,6 +1,11 @@
+'use client'
+
+import NavbarWithSideMenu from '@/components/nav-and-sidemenu/navbar-with-sidemenu'
+
 const MockStudentPage = () => {
   return (
     <div>
+      <NavbarWithSideMenu className="student" displaySideMenu={false} />
       <h1>Student Page</h1>
     </div>
   )
