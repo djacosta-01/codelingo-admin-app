9730cfa8493d6b42edc8aba0df4dc5a4f8d1c859
djacosta-01 <djacosta.07@gmail.com>
2024-11-14 19:13:16 -0800

    edited these pages

--- client/app/student/page.tsx
+++ client/app/student/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 
 import NavbarWithSideMenu from '@/components/nav-and-sidemenu/navbar-with-sidemenu'
+import { Box } from '@mui/material'
 
 const MockStudentPage = () => {
   return (
-    <div>
-      <NavbarWithSideMenu className="student" displaySideMenu={false} />
-      <h1>Student Page</h1>
-    </div>
+    <>
+      {/* <NavbarWithSideMenu className="student" displaySideMenu={false} /> */}
+      <Box sx={{ marginTop: '64px', marginLeft: '65px' }}>
+        <h1>Student Page</h1>
+      </Box>
+    </>
   )
 }
 
