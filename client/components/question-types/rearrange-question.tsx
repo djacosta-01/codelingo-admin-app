a626aa311914d2953e882ff0b1f67cb0e93bdec7
djacosta-01 <djacosta.07@gmail.com>
2024-12-10 22:49:54 -0800

    able to tokenize successfully from the editor

--- client/components/question-types/rearrange-question.tsx
+++ client/components/question-types/rearrange-question.tsx
@@ -15,10 +15,9 @@ const RearrangeQuestion = () => {
 
   const handleSelectionChange = () => {
     const selection = window.getSelection()
-    // alert('selection: ' + selection)
-    if (selection) {
-      console.log(selection)
+    if (selection && selection.toString().length > 0) {
       setSelectedText(selection.toString())
+      setTokens(prev => [...prev, selection.toString()])
     } else {
       alert('No text selected')
       // setSelectedText('')
@@ -46,69 +45,63 @@ const RearrangeQuestion = () => {
   }
 
   return (
-    <Box
-      onContextMenu={handleContextMenu}
-      sx={{ cursor: 'context-menu', backgroundColor: 'pink', height: '100vh' }}
-    >
-      {snippetIncluded ? (
-        <>
-          <Editor
-            theme="vs-dark"
-            height="50vh"
-            width="50vw"
-            defaultLanguage="python"
-            options={{ contextmenu: false }}
-            value={questionPrompt}
-            onChange={(value, event) => handleEditorChange(value, event)}
-          />
-          <IconButton color="error" onClick={() => setSnippetIncluded(false)}>
-            <RemoveIcon />
-          </IconButton>
-        </>
-      ) : (
-        <Box>
-          <Button onClick={() => setSnippetIncluded(true)}>Add Snippet</Button>
-        </Box>
-      )}
-
-      <p>TEST</p>
-      <p>Select some text below:</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <Menu
-        open={contextMenu !== null}
-        onClose={handleClose}
-        anchorReference="anchorPosition"
-        anchorPosition={
-          contextMenu !== null ? { top: contextMenu.mouseY, left: contextMenu.mouseX } : undefined
-        }
-      >
-        <MenuItem onClick={handleSelectionChange}>Create Token</MenuItem>
-        <MenuItem onClick={handleClose}>Cut</MenuItem>
-      </Menu>
-
-      <Box>
-        {/* {tokens.map((token, index) => (
-          <Paper key={index} sx={{ padding: 1, margin: 1 }}>
+    <>
+      <Box onContextMenu={handleContextMenu} sx={{ cursor: 'context-menu' }}>
+        <div>UNDER CONSTRUCTION</div>
+        {snippetIncluded ? (
+          <>
+            <Editor
+              theme="vs-dark"
+              height="50vh"
+              width="50vw"
+              defaultLanguage="python"
+              options={{ contextmenu: false }}
+              value={questionPrompt}
+              onChange={(value, event) => handleEditorChange(value, event)}
+            />
+            <IconButton color="error" onClick={() => setSnippetIncluded(false)}>
+              <RemoveIcon />
+            </IconButton>
+          </>
+        ) : (
+          <Box>
+            <Button onClick={() => setSnippetIncluded(true)}>Add Snippet</Button>
+          </Box>
+        )}
+        <Menu
+          open={contextMenu !== null}
+          onClose={handleClose}
+          anchorReference="anchorPosition"
+          anchorPosition={
+            contextMenu !== null ? { top: contextMenu.mouseY, left: contextMenu.mouseX } : undefined
+          }
+        >
+          <MenuItem onClick={handleSelectionChange}>Create Token</MenuItem>
+          <MenuItem onClick={handleClose}>Cut</MenuItem>
+        </Menu>
+      </Box>
+      <Box sx={{ display: 'flex', gap: 2 }}>
+        {tokens.map((token, index) => (
+          <Paper
+            elevation={4}
+            key={index}
+            sx={{
+              padding: 1,
+              height: '5em',
+              minWidth: `5em`,
+              maxWidth: `calc(${selectedText.length}em / 2)`,
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+            }}
+          >
             {token}
           </Paper>
-        ))} */}
-        <Paper
-          elevation={4}
-          sx={{
-            padding: 1,
-            height: '5em',
-            minWidth: `5em`,
-            maxWidth: `calc(${selectedText.length}em / 2)`,
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexWrap: 'wrap',
-          }}
-        >
-          <p>{selectedText}</p>
-        </Paper>
+        ))}
       </Box>
-    </Box>
+      <Button onClick={() => setTokens([])}>CLEAR</Button>
+    </>
   )
 }
 
